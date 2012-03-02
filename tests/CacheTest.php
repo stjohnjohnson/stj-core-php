5063dc9c5d61da3412f5ff9ab1474a65648bcd95
St. John Johnson <st.john.johnson@gmail.com>
2012-03-02 10:28:35 -0800

    Changed the Cache test around a bit

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -15,16 +15,24 @@ class CacheTest extends PHPUnit_Framework_TestCase {
   /**
    * @test
    * @group Cache
-   * @group Cache.storeAndFetch
+   * @group Cache.store
    * @covers stj\Cache
+   * @dataProvider cacheProvider
    */
-  public function storeAndFetch() {
-    $key = __METHOD__;
-    $value = 'phpunit test value';
-    $this->assertFalse(apc_fetch($key));
-    $this->assertFalse(Cache::get($key));
-
+  public function store($key, $value) {
     Cache::set($key, $value);
+    $this->assertSame($value, apc_fetch($key));
+  }
+
+  /**
+   * @test
+   * @group Cache
+   * @group Cache.retrieve
+   * @covers stj\Cache
+   * @dataProvider cacheProvider
+   */
+  public function retrieve($key, $value) {
+    $this->store($key, $value);
     $this->assertSame($value, apc_fetch($key));
     $this->assertSame($value, Cache::get($key));
   }
@@ -34,17 +42,28 @@ class CacheTest extends PHPUnit_Framework_TestCase {
    * @group Cache
    * @group Cache.delete
    * @covers stj\Cache
+   * @dataProvider cacheProvider
    */
-  public function delete() {
-    $key = __METHOD__;
-    $value = 'phpunit test value';
-
-    Cache::set($key, $value);
-    $this->assertSame($value, apc_fetch($key));
-    $this->assertSame($value, Cache::get($key));
+  public function delete($key, $value) {
+    $this->retrieve($key, $value);
 
     Cache::delete($key);
     $this->assertFalse(apc_fetch($key));
     $this->assertFalse(Cache::get($key));
+  }
+
+  /**
+   * Data Provider for Cache Tests
+   * 
+   * @return array
+   */
+  public function cacheProvider() {
+    $cases = array();
+
+    foreach (range(0, 10) as $i) {
+      $cases[] = array(__METHOD__ . $i, mt_rand(110, 99999));
+    }
+
+    return $cases;
   }
 }
